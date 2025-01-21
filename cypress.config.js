6732a38ffa416ee3d4d607bbc193d6d1d5dbef7f
Andre Hildinger <andrehil@gmail.com>
2025-01-21 14:41:18 -0500

    Generate Cypress report

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,12 @@
 const config = {
     e2e: {},
+    reporter: 'mochawesome',
+    reporterOptions: {
+        reportDir: 'cypress/results',
+        overwrite: false,
+        html: false,
+        json: true,
+    }
 };
 
 export default config;
